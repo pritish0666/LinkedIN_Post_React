918eded5ff773110249fb18f7b80fb29c5e21f9f
Pritish <pritishpatra06@gmail.com>
2024-10-10 00:37:13 +0530

    final fixes

--- Extension/popup.js
+++ Extension/popup.js
@@ -1,18 +1,10 @@
 const like = document.querySelector(".cbtn");
-const comment = document.querySelector(".cmbtn");
 const btnv = document.querySelector(".btn");
 const go = document.querySelector(".gobtn");
 
+// Show button if like input is filled
 like.addEventListener("keyup", function () {
-  if (like.value != "" && comment.value != "") {
-    btnv.classList.remove("hidden");
-  } else {
-    btnv.classList.add("hidden");
-  }
-});
-
-comment.addEventListener("keyup", function () {
-  if (like.value != "" && comment.value != "") {
+  if (like.value != "") {
     btnv.classList.remove("hidden");
   } else {
     btnv.classList.add("hidden");
@@ -23,6 +15,7 @@ go.addEventListener("click", function () {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     var tab = tabs[0];
 
+    // Navigate to LinkedIn feed page
     chrome.tabs.update(tab.id, { url: "https://www.linkedin.com/feed/" });
   });
 
@@ -33,9 +26,9 @@ go.addEventListener("click", function () {
   ) {
     console.log(message);
 
+    // Send only like count data, no comment count
     sendResponse({
       data: like.value,
-      data1: comment.value,
     });
   });
 });
